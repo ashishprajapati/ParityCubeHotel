26a04d446614e46b4954a5876458ab3d8a320d4c
Ashish Prajapati <ashishprajapati0404@gmail.com>
2016-10-09 01:43:31 +0530

    Features# Registration controller updated

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
-  
-  protected
-
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
 
   private
 
+  # Sign up params for creating user
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :mobile, :password, :password_confirmation)
   end
 
+  # Accoung update params required while updating a user
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :mobile, :password, :password_confirmation, :current_password)
   end
